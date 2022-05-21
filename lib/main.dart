7d63c4e74512d4afd0b897a4cc0eea5bbda010d0
Astraxx04 <gagan200254@gmail.com>
2022-05-21 20:26:36 +0530

    Small updates

--- lib/main.dart
+++ lib/main.dart
@@ -1,52 +1,13 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
-
-class MyApp extends StatefulWidget {
-  @override
-  State<StatefulWidget> createState() {
-    // TODO: implement createState
-    return MyAppState();
-  }
+void main() {
+  runApp(MyApp());
 }
 
-class MyAppState extends State<MyApp> {
-  var questionIndex = 0;
-
-  void answer() {
-    setState(() {
-      questionIndex++;
-    });
-    print(questionIndex);
-  }
-
-  var questions = [
-    'What\'s your favourite colour?',
-    'What\'s your favourite animal?'
-  ];
-
-  @override
+class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(title: Text('My First App')),
-        body: Column(children: [
-          Text('Answer the questions'),
-          Text(questions[questionIndex]),
-          ElevatedButton(
-            child: Text('3'),
-            onPressed: answer,
-          ),
-          ElevatedButton(
-            child: Text('2'),
-            onPressed: answer,
-          ),
-          ElevatedButton(
-            child: Text('4'),
-            onPressed: answer,
-          ),
-        ]),
-      ),
+      home: Text('Lets Start!!'),
     );
   }
 }
