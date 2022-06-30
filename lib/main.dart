0f2d3624c379cd910b59438f05e5e048ff46ad74
Astraxx04 <gagan200254@gmail.com>
2022-06-30 07:16:51 +0530

    Very little changes are done!!

--- lib/main.dart
+++ lib/main.dart
@@ -5,9 +5,31 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
+  var queindex = 0;
+  void answerQuestion() {
+    queindex = queindex + 1;
+    print(queindex);
+  }
+
+  @override
   Widget build(BuildContext context) {
+    var ques = ['Fav Colour', 'Fav Car', 'Fav Chocolate'];
     return MaterialApp(
-      home: Text('Lets Start!!'),
+      home: Scaffold(
+          appBar: AppBar(
+            title: Text('My first app'),
+          ),
+          body: Column(
+            children: [
+              Text(ques[queindex]),
+              ElevatedButton(
+                  onPressed: answerQuestion, child: Text('Answer 1')),
+              ElevatedButton(
+                  onPressed: answerQuestion, child: Text('Answer 2')),
+              ElevatedButton(
+                  onPressed: answerQuestion, child: Text('Answer 3')),
+            ],
+          )),
     );
   }
 }
